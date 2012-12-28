69cdbc6eae4b7cb45f46cdf915b167c84871d14c
Jonathan Friedman <friedmanj98@gmail.com>
2012-12-27 20:48:41 -0500

    Bug Fix: heroku better errors bug

--- main.rb
+++ main.rb
@@ -1,12 +1,15 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
-require "better_errors"
+
+#set better errors for testing
+if ENV['RACK_ENV'] != 'production'
+  require "better_errors"
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path("..", __FILE__)
+end
 
 set :haml, :format => :html5
-
-use BetterErrors::Middleware
-BetterErrors.application_root = File.expand_path("..", __FILE__)
 
 get '/' do
   haml :index
