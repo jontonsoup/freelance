8b622da265c3e4c84c4dfd68be96c20a5a0791b2
Jonathan Friedman <friedmanj98@gmail.com>
2012-12-27 19:40:20 -0500

    Feature: better errors

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'http://rubygems.org'
 gem 'sinatra'
 gem 'thin'
 gem 'haml'
+gem "binding_of_caller"
+
+group :development do
+  gem "better_errors"
+end
--- main.rb
+++ main.rb
@@ -1,8 +1,12 @@
 require 'rubygems'
-require 'sinatra'  
+require 'sinatra'
 require 'haml'
+require "better_errors"
 
 set :haml, :format => :html5
+
+use BetterErrors::Middleware
+BetterErrors.application_root = File.expand_path("..", __FILE__)
 
 get '/' do
   haml :index
