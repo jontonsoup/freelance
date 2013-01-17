ef0e1e11b276a876e3bd6343eebeca26a8fb2329
Jonathan Friedman <friedmanj98@gmail.com>
2013-01-17 15:04:58 -0600

    Bug Fix: static asset cache v4

--- main.rb
+++ main.rb
@@ -16,7 +16,7 @@ set :haml, :format => :html5
 configure do
   use Rack::Deflater
   use Rack::Cache
-  set :static_cache_control, [:public, :max_age => 360000]
+  set :static_cache_control, [:public, :max_age => 760000]
 end
 
 
